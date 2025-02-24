9be85d91018066560890347c0b73e0f2caa0412e
ImadlakQvQ <472974582@qq.com>
2025-02-24 17:29:07 -0500

    .

--- job_memt.sh
+++ job_memt.sh
@@ -21,7 +21,6 @@ python run_minigrid_ppo_memt.py  --n_exp 9 --name TEST_ --env_name ${ENV_NAME} -
                             --n_action_experts ${N_EXPERTS} --actor_network MiniGridPPOMEMTNetwork --actor_n_features 128 \
                             --batch_size 256 --gamma 0.99 --coeff_experts ${COEFF} --save --wandb
 
-
 # python run_minigrid_ppo_memt.py  --n_exp 1 --name TEST_ --env_name MT3 --exp_name debug \
 #                             --n_epochs 2 --n_steps 2000  --n_episodes_test 16 --train_frequency 2000 --lr_actor 6e-3 --lr_critic 1e-3 \
 #                             --critic_network MiniGridPPOMEMTNetwork --critic_n_features 128 --orthogonal --n_experts 4 \
@@ -33,7 +32,6 @@ python run_minigrid_ppo_memt.py  --n_exp 9 --name TEST_ --env_name ${ENV_NAME} -
 #                             --critic_network MiniGridPPOMixtureMHNetwork --critic_n_features 128 --orthogonal --n_experts ${N_EXPERTS} \
 #                             --actor_network MiniGridPPOMixtureMHNetwork --actor_n_features 128 \
 #                             --batch_size 256 --gamma 0.99 --wandb 
-
 
 # python run_minigrid_ppo_mt.py  --n_exp 15 --name MEMT\
 #                             --env_name ${ENV_NAME} --exp_name ppo_mt_moore_multihead_${N_EXPERTS}e \
