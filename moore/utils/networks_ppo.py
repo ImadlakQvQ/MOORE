10cc124ba889db384549dfc202234cad84e2c944
ImadlakQvQ <imadlak@foxmail.com>
2025-02-24 14:42:41 -0500

    .

--- moore/utils/networks_ppo.py
+++ moore/utils/networks_ppo.py
@@ -299,6 +299,12 @@ class MiniGridPPOMixtureMHNetwork(nn.Module):
 
         return torch.permute(feat, (1,0,2)).cpu().numpy()
 
+    def save_params(self, save_dir):
+        torch.save(self.state_dict(), save_dir)
+
+    def load_params(self, load_dir):
+        self.load_state_dict(torch.load(load_dir, map_location="cuda" if self._use_cuda else "cpu"))
+
     def save_shared_backbone(self, save_dir):
         torch.save(self.cnn.state_dict(), save_dir)
     
